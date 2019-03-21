2428b73dab646996726a316c2a21faed0f49b3cd
Linda <llmitton@gmail.com>
2019-03-20 22:34:49 -0300

    Created the initial display and list
    Got the list to display, however it is an item per page

---
+++ RecipeBasic/app/src/main/java/com/example/recipebasic/MainActivity.java
@@ -0,0 +1,69 @@
+//Linda Mitton ~ 19 March 2019
+
+package com.example.recipebasic;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinkedList<Recipe> recipes = new LinkedList<>();
+    private RecyclerView rRecyclerView;
+    private RecipeAdapter rAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //establish the data list to display
+        recipes = DataProvider.getRecipes();
+
+        //Get the recyclerview id
+        rRecyclerView = findViewById(R.id.recycler_view);
+
+        //create an adapter and supply the data to be displayed
+        rAdapter = new RecipeAdapter(this, recipes);
+
+        //connect the adapter with the recycler view
+        rRecyclerView.setAdapter(rAdapter);
+
+        //provide the recyclerview with a default layout manager
+        rRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ RecipeBasic/app/src/main/java/com/example/recipebasic/RecipeAdapter.java
@@ -0,0 +1,64 @@
+package com.example.recipebasic;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+
+    private final LinkedList<Recipe> recipeList;
+    private LayoutInflater rInflater;
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView rName;
+        public final TextView rDesc;
+        final RecipeAdapter rAdapter;
+
+        public RecipeViewHolder(View itemView, RecipeAdapter adapter) {
+            super(itemView);
+            rName = itemView.findViewById(R.id.recipe_name);
+            rDesc = itemView.findViewById(R.id.recipe_desc);
+            this.rAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int rPosition = getLayoutPosition();
+            //Recipe recipe = recipeList.get(rPosition);
+            rAdapter.notifyItemChanged(rPosition);
+        }
+    }
+
+    public RecipeAdapter(Context context, LinkedList<Recipe> rList) {
+        rInflater = LayoutInflater.from(context);
+        this.recipeList = rList;
+    }
+
+    @NonNull
+    @Override
+    public RecipeAdapter.RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View rCardView = rInflater.inflate(R.layout.recipelist_item, parent, false);
+
+        return new RecipeViewHolder(rCardView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeAdapter.RecipeViewHolder recipeViewHolder, int position) {
+        Recipe recipe = recipeList.get(position);
+        recipeViewHolder.rName.setText(recipe.name);
+        recipeViewHolder.rDesc.setText(recipe.description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeList.size();
+    }
+}
